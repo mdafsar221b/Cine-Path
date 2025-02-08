ac55fc567ec63833367f3b2c7aab50b8e4bb549c
Md Afsar <afsarsayyyed@gmail.com>
2025-02-09 01:03:20 +0530

    Use imdb api because tmdb server is down

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,58 @@ import React, {useEffect, useState } from 'react';
 import Search from './components/Search';
 
 
-const API_BASE_URL= 'https://tmdb-movies-and-tv-shows-api-by-apirobots.p.rapidapi.com/v1/tmdb?name=robot&page=1';
 
-const API_OPTIONS = {
-   headers: {
+const url = 'https://imdb236.p.rapidapi.com/imdb/most-popular-movies';
+const options = {
+  method: 'GET',
+  headers: {
     'x-rapidapi-key': '0c1683b310mshe96ed35887ff7dcp14b057jsn08e315a7a60c',
-    'x-rapidapi-host': 'tmdb-movies-and-tv-shows-api-by-apirobots.p.rapidapi.com'
+    'x-rapidapi-host': 'imdb236.p.rapidapi.com'
   }
-
 };
 
+/*
+const API_BASE_URL= 'https://imdb236.p.rapidapi.com/imdb/most-popular-movies';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+   
+    headers:{
+      accept:'application/json',
+       'x-rapidapi-host': 'imdb236.p.rapidapi.com',
+      Authorization:`Bearer ${API_KEY}`
+    }
+
+};
+*/
+
+
 const App = () => {
+  
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isloading, setIsLoading] = useState(false);
-  const fetchMovies=async () => {
+  const fetchMovies= async () => {
     setIsLoading(true);
     setErrorMessage('');
-    try{
-      const endpoint =`${API_BASE_URL}`;
 
-      const response = await fetch(endpoint,API_OPTIONS);
+    try{
+
+      const response = await fetch(url, options);
      if(!response.ok){
       throw new Error('Failed to fetch data');
      }
      const data = await response.json();
      console.log(data);
+
      if(data.response === 'false'){
       setErrorMessage(data.error || 'Failed to fetch movies');
       setMovieList([]);
       return;
      }
-      setMovieList(data.results || []);
+      setMovieList(data|| []);
     } catch(error) {
       console.error(`Error fetching movies : ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
@@ -46,6 +65,7 @@ const App = () => {
    useEffect(() =>{
     fetchMovies();
    },[]);
+   
 
   return (
     <main>
@@ -56,10 +76,27 @@ const App = () => {
           <h1>
             Find <span className='text-gradient'>Movie </span> You'll Enjoy Without the Hassle
           </h1>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <Search/>
         </header>
         <section className='all-movies'>
-          <h2>All Movies</h2>
+          <h2>Popular Movies</h2>
+          <ul>
+             
+             {movieList.map((movie) => (
+              
+              <div key={movie.id} className='movie-card flex flex-col justify-between'>
+                <img src={movie.primaryImage} alt="" />
+                <h3 className='text-white'>{movie.originalTitle}</h3>
+                <div>
+                  <p className='text-white py-0.5'> Year : {movie.startYear}</p>
+                  <p className='text-white py-0.5'> Lan : {movie.language}</p>
+                </div>
+
+              </div>
+               
+             ))}
+           </ul>
+         
          {isloading ? (
           <p className='text-white'> Loading......</p>
          ):errorMessage ?(
@@ -67,14 +104,14 @@ const App = () => {
          ) :(
           <ul>
              
-            {movieList.map((movie) => (
-              <p className='text-white'>{movieList}</p>
+            { movieList.map((movie,index) => (
+              <p key={index} className='text-white'>{movie.title}</p>
               
             ))}
           </ul>
          )
         }
-          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+          {errorMessage && <p className='text-red-500'>{errorMessage}</p>} 
 
         </section>
       </div>
