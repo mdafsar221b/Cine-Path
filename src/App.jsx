167ccb28f77e2dca15eedcc0ef79fe8529d38a7d
Md Afsar <afsarsayyyed@gmail.com>
2025-02-08 14:44:34 +0530

    Integrate TMDb API for fetching movie data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import Search from './components/Search';
+
+
+const API_BASE_URL= 'https://tmdb-movies-and-tv-shows-api-by-apirobots.p.rapidapi.com/v1/tmdb?name=robot&page=1';
+
+const API_OPTIONS = {
+   headers: {
+    'x-rapidapi-key': '0c1683b310mshe96ed35887ff7dcp14b057jsn08e315a7a60c',
+    'x-rapidapi-host': 'tmdb-movies-and-tv-shows-api-by-apirobots.p.rapidapi.com'
+  }
+
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  const fetchMovies=async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+    try{
+      const endpoint =`${API_BASE_URL}`;
+
+      const response = await fetch(endpoint,API_OPTIONS);
+     if(!response.ok){
+      throw new Error('Failed to fetch data');
+     }
+     const data = await response.json();
+     console.log(data);
+     if(data.response === 'false'){
+      setErrorMessage(data.error || 'Failed to fetch movies');
+      setMovieList([]);
+      return;
+     }
+      setMovieList(data.results || []);
+    } catch(error) {
+      console.error(`Error fetching movies : ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later.');
+    } finally{
+      setIsLoading(false);
+    }
+  }
+   
+   useEffect(() =>{
+    fetchMovies();
+   },[]);
+
   return (
     <main>
       <div className='pattern' >
@@ -12,9 +56,27 @@ const App = () => {
           <h1>
             Find <span className='text-gradient'>Movie </span> You'll Enjoy Without the Hassle
           </h1>
-        </header>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className='text-white'>{searchTerm}</h1>
+        </header>
+        <section className='all-movies'>
+          <h2>All Movies</h2>
+         {isloading ? (
+          <p className='text-white'> Loading......</p>
+         ):errorMessage ?(
+          <p className='text-red-500'>{errorMessage}</p>
+         ) :(
+          <ul>
+             
+            {movieList.map((movie) => (
+              <p className='text-white'>{movieList}</p>
+              
+            ))}
+          </ul>
+         )
+        }
+          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+
+        </section>
       </div>
       </div>
     </main>
